4b9f7ee618e445735b500a37e7ccdb80d1f26042
rashu-pro <rashu.pro@gmail.com>
2024-01-23 16:13:27 +0600

    initial commit

--- functions.php
+++ functions.php
@@ -1,6 +1,17 @@
 <?php
-add_action('wp_enqueue_scripts', 'divi_child_enqueue_styles');
-function divi_child_enqueue_styles()
+function dt_enqueue_styles()
 {
-  wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/style.css');
+  $parenthandle = 'divi-style';
+  $theme = wp_get_theme();
+  wp_enqueue_style($parenthandle, get_template_directory_uri() . '/style.css',
+    array(), // if the parent theme code has a dependency, copy it to here
+    $theme->parent()->get('Version')
+  );
+
+  wp_enqueue_style('child-style', get_stylesheet_uri(),
+    array($parenthandle),
+    $theme->get('Version')
+  );
 }
+
+add_action('wp_enqueue_scripts', 'dt_enqueue_styles');
